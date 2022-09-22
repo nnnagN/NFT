a685a090debd5ce9398a3ea6ad16403213fb4078
AnhQuan9x99m <quannguyen1999qt@gmail.com>
2022-09-22 17:54:03 +0700

    push 2

--- src/containers/Layout/Header/index.tsx
+++ src/containers/Layout/Header/index.tsx
@@ -1,25 +1,70 @@
-import BackToTop from '@components/Button/BackToTop';
-import Footer from './Footer';
-import Header from './Header';
-import styles from './index.module.scss';
+import Link from 'next/link';
+import Image from '../../../components/Image';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 type Props = {
-  children: React.ReactNode
+
 };
 
-const Layout: React.FC<Props> = ({children}) => {
+const Header: React.FC<Props> = () => {
+
+  const listMenu = [
+    {name: 'Home', link: '/#home'},
+    {name: 'About', link: '/#about'},
+    {name: 'Features', link: '/#features'},
+    {name: 'Screenshot', link: '/#screenshot'},
+    {name: 'Testimonial', link: '/#testimonial'},
+    {name: 'Pricing', link: '/#pricing'},
+    {name: 'Faq', link: '/#faq'},
+    {name: 'Contact Us', link: '/#contact'},
+  ];
+
+  const router = useRouter();
+  const [path, setPath] = useState('');
+
+  useEffect(() => {
+    setPath(router.asPath);
+  }, [router.asPath]);
 
   return (
-    <div id="main-wrapper" className={styles.layout}>
-      <Header />
-      <div className="content">
-        {children}
-      </div>
-      <Footer />
+    <header id="header" className="sticky-top-slide">
+      <nav className="primary-menu navbar navbar-expand-lg fw-600 sticky-on">
+        <div className="container-lg">
+          <Link href="/">
+            <a><Image src="public/logo.png" className="header-logo" width={125} height={35} /></a>
+          </Link>
 
-      <BackToTop />
-    </div>
+          <button type="button" data-bs-toggle="collapse" data-bs-target="#header-nav" className="navbar-toggler collapsed" aria-expanded="false">
+            <span />
+            <span />
+            <span />
+          </button>
+
+          <div id="header-nav" className="collapse navbar-collapse justify-content-end">
+            <ul className="navbar-nav">
+              {listMenu.map((menu, idx) => (
+                <li className="nav-item" key={idx} onClick={() => setPath(router.asPath)}>
+                  <Link href={menu.link}>
+                    <a className={`nav-link ${menu.link === path ? 'active' : ''}`}>
+                      {menu.name}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+              <li className="align-items-center h-auto ms-lg-3">
+                <Link href="#download">
+                  <a className="nav-link btn btn-primary link-light text-3 fw-600 shadow-none d-inline-block mt-3 mt-lg-0 smooth-scroll">
+                    Download
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
-export default Layout;
+export default Header;
