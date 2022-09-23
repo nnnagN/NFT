88d4b93e6b9fd371c7f0fcf64d9690ac9331a455
AnhQuan9x99m <quannguyen1999qt@gmail.com>
2022-09-23 09:30:40 +0700

    push again

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,35 +1,23 @@
-import About from '@containers/Home/About/Index';
-import Contact from '@containers/Home/Contact/Index';
-import Counter from '@containers/Home/Counter';
-import Faq from '@containers/Home/Faq/Index';
-import Feature from '@containers/Home/Feature';
-import Feedback from '@containers/Home/Feedback';
-import GetTheApp from '@containers/Home/GetTheApp';
-import HowItWorks from '@containers/Home/HowItWorks';
-import Intro from '@containers/Home/Intro';
-import Pricing from '@containers/Home/Pricing';
-import AppScreens from '@containers/Home/AppScreens';
-import Subscribe from '@containers/Home/Subscribe';
-import type { NextPage } from 'next';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-const Home: NextPage = () => {
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="App landing page" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-  return (
-    <>
-      <Intro />
-      <About />
-      <Counter />
-      <Feature />
-      <AppScreens />
-      <HowItWorks />
-      <Feedback />
-      <Pricing />
-      <GetTheApp />
-      <Faq />
-      <Subscribe />
-      <Contact />
-    </>
-  );
-};
-
-export default Home;
+export default MyDocument;
